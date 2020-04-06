1db1881ea7d72c84557cc563076da80e616994d5
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-06 16:08:38 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-class Kid 
+class Kid
+  include Dance 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
 end 
